0f27caef33aa0a8d6dbe85a573bb042286023378
Amit42373 <amit42373@gmail.com>
2024-11-18 15:10:32 +0530

    Update utils.py

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -7,9 +7,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY", "x7dE9nBq4kJ3mA2zR1L8vX9sW5tFqY0zP6dG")
-ALGORITHM = os.getenv("ALGORITHM", "HS256")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+SECRET_KEY = "x7dE9nBq4kJ3mA2zR1L8vX9sW5tFqY0zP6dG"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -20,6 +20,7 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
 def create_access_token(data: dict) -> str:
+    print({ SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES });
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
