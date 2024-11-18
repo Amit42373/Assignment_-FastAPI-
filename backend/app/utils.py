1b04e5f29c170217f9e73aede78d98e2effb7890
Amit42373 <amit42373@gmail.com>
2024-11-18 14:17:10 +0530

    Update utils.py

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -23,5 +23,7 @@ def create_access_token(data: dict) -> str:
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    jwtstring = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    print('jwtstring', jwtstring)
+    return jwtstring
 
